6f5e3cea736e6c95837f11053e261944a4996be3
B4-Store <132667154+B4Store@users.noreply.github.com>
2023-05-06 18:09:57 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,33 @@
+Config = {}
+
+QBCore = exports['qb-core']:GetCoreObject()
+
+Config.drugs = 'qb-drugs'
+Config.methlab = 'qb-methlab'
+
+Config = {
+    AdminsCID = { -- Menu GOD
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+    },
+    DevsCID = { -- Menu GOD
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+        'put your citizen id', -- iDX - B4 Store
+    }
+}
